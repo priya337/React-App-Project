85132046ba2b0ce1dbca16ea563e7fe77724bb96
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-10-31 15:29:20 +0100

    Added the Sidebar component for Kanban Dashboard

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,19 @@
 // App.jsx
 import React from 'react';
-import KanbanBoard from './Sidebar';
+import Sidebar from './Sidebar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="app-container">
-      <KanbanBoard />
+      <Sidebar />
+      {/* Other main content here */}
+      {/* <div className="main-content"> */}
+        {/* Placeholder for the rest of the application content */}
+        {/* <h1>Main Content</h1> */}
+      {/* </div> */}
     </div>
   );
-}
+};
 
 export default App;
