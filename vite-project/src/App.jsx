7b9ddb77befb4f92311763d1dc5dcffdf294f601
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-10-31 20:53:27 +0100

    App.jsx merge issues

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,19 @@
 import React from 'react';
 import KanbanBoard from './Sidebar';
 import './App.css';
+import Navbar from './components/Navbar.jsx';
 
 const App = () => {
   return (
     <div className="app-container">
-      <KanbanBoard />
+      <Sidebar />
+      <Navbar />
+      {/* Other main content here */}
+      {/* <div className="main-content"> */}
+        {/* Placeholder for the rest of the application content */}
+        {/* <h1>Main Content</h1> */}
+      {/* </div> */}
     </div>
   );
 };
->>>>>>> 85132046ba2b0ce1dbca16ea563e7fe77724bb96
-
 export default App;
