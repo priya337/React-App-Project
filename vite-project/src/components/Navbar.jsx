3b04c02907979bcf1a5ec3f972b20f98f26c5cff
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-11-02 16:02:54 +0100

    Modified Sidebar, Added Columns and display cards, made the display cards responsive to the connected board in the Sidebar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -5,14 +5,9 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-<<<<<<< HEAD
       <div className="navbar-logo">
         <img src="./Calendar.PNG" alt="Logo" />
         <span className="navbar-title">Kanban Dashboard</span>
-=======
-      <div className="logo">
-        <h1>Kanban Dashboard</h1> {}
->>>>>>> 8791025 (Modified Sidebar, Added Columns and display cards, made the display cards responsive to the connected board in the Sidebar)
       </div>
     </nav>
   );
