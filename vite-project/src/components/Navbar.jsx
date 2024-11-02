bbbb0cd4bfd3239b115b173647b3add5a0805271
Anna Turek <anutkakpi@ukr.net>
2024-11-02 10:25:38 +0200

    added Navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -6,14 +6,8 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img src="./components/Calendar.PNG" alt="Logo" />
+        <img src="./Calendar.PNG" alt="Logo" />
         <span className="navbar-title">Kanban Dashboard</span>
-        <nav className="menu">
-          <div className="navbar-menu">
-            <img src="./components/menu-icon.png" alt="Menu" />
-            <button className="menu-button">onClick={toggleMenu}</button>
-          </div>
-        </nav>
       </div>
     </nav>
   );
