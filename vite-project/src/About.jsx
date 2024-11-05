5f65a689c71bb1afef15c51956805546f4d488f5
Mirela Dragulescu <dragulescumirela@gmail.com>
2024-11-05 16:30:21 +0100

    improved the homepage and about page

--- vite-project/src/About.jsx
+++ vite-project/src/About.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Priya from "../public/Priya.png";
+import Anna from "../public/Anna.png";
+import Mirela from "../public/Mirela.png";
+import "./About.css";
 
 function About() {
   return (
@@ -6,9 +10,21 @@ function About() {
       <div className="about-container">
         <h1>Meet the team!</h1>
         <ul className="about-list-items">
-          <li>Priya</li>
-          <li>Mirela</li>
-          <li>Anna</li>
+          <li>
+            <img src={Priya} alt="profile" className="profile-image" />
+            <br />
+            Priya
+          </li>
+          <li>
+            <img src={Mirela} alt="profile" className="profile-image" />
+            <br />
+            Mirela
+          </li>
+          <li>
+            <img src={Anna} alt="profile" className="profile-image" />
+            <br />
+            Anna
+          </li>
         </ul>
       </div>
     </>
--- vite-project/src/Homepage.jsx
+++ vite-project/src/Homepage.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import "./Homepage.css";
+import logo from "../public/ironhack logo.png";
+import Kanban from "../public/kanban-homepage.png";
 
 function Homepage() {
   return (
     <div className="container">
+      <img src={logo} alt="logo" className="logo-ironhack" />
       <h1>Welcome to Ironhack Kanban Board!</h1>
       <p>
         Try our popular framework used to implement Agile and DevOps software
         development.
       </p>
-      <br />
       <p>
-        Clear organisation and priority with grouped lists: To Do, In Progress
-        and Done
+        Clear organisation and priority with grouped lists:{" "}
+        <strong>
+          <span style={{ color: "red" }}>To Do</span>
+        </strong>
+        ,{" "}
+        <strong>
+          <span style={{ color: "orange" }}>In Progress</span>
+        </strong>{" "}
+        and{" "}
+        <strong>
+          <span style={{ color: "green" }}>Done</span>
+        </strong>
       </p>
+      <br />
+      <img src={Kanban} alt="kanban" className="kanban-homepage" />
     </div>
   );
 }
