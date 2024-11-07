bf44f2ecfc3f580edd16cc63c29b8c2b2e026938
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-11-07 13:16:54 +0100

    Modified Navbar , Sidebar and TaskDetails components and associated styling files

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,20 +1,19 @@
-//Navbar.jsx
-import { Link } from "react-router-dom";
+// Navbar.jsx
+import { NavLink } from "react-router-dom";
 import React from "react";
 import "./Navbar.css";
-import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className="navbar-links">
+        <NavLink to="/">Homepage</NavLink>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </div>
       <div className="navbar-logo">
         <img src="./Calendar.PNG" alt="Logo" />
         <span className="navbar-title">Kanban Dashboard</span>
-        <div className="links">
-          <NavLink to="/">Homepage</NavLink>
-          <NavLink to="/dashboard">Dashboard</NavLink>
-          <NavLink to="/About">About</NavLink>
-        </div>
       </div>
     </nav>
   );
