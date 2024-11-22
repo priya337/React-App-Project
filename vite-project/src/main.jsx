6b0153a1697269a7314677000a0ad8831370ee78
Priya Sidhaiyan <priyasidhaiyan@priyas-mbp.home>
2024-11-22 02:28:10 +0100

    Modified main , app components  and vite.config.js for the purpose correct link routing

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter without HashRouter or basename
 import App from './App';
 import './index.css';
 
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/React-App-Project/' // Replace 'repo-name' with your actual repository name
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/React-App-Project/' : '/', // Dynamic base path for dev and production
+  };
 });
